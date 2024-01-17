7b729accfc1981f1e71e24e62166ce16918d90e0
ian <iansu.twn@gmail.com>
2024-01-17 23:10:44 +0800

    add pandas solution

--- 1683.sql
+++ 1683.sql
@@ -3,3 +3,7 @@ where length(ifnull(content, 0)) > 15
 
 select tweet_id from Tweets
 where length(coalesce(content, 0)) > 15
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    res = tweets[(tweets["content"].str.len() > 15) | (tweets["content"].isna())]
+    return res[["tweet_id"]]
