b4ce8b7d78fa7cb9b663351950d4f911a7b24fa2
ian0521 <iansu.twn@gmail.com>
2024-01-21 00:28:45 +0800

    add another sql solution

--- 1934.sql
+++ 1934.sql
@@ -1,8 +1,17 @@
+-- method 1
 select s.user_id,
 round(sum(
     case when action = "confirmed" then 1 else 0 end
     )/count(*)
 , 2) confirmation_rate
+from signups s
+left join confirmations c
+on c.user_id = s.user_id
+group by s.user_id
+
+-- method 2
+select s.user_id,
+round(avg(if(action = "confirmed", 1, 0), 2)) as confirmation_rate
 from signups s
 left join confirmations c
 on c.user_id = s.user_id
