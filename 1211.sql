93f009a8423b817ebdaf5cf4d0c6939fbb84ac72
ian0521 <iansu.twn@gmail.com>
2024-01-21 21:50:08 +0800

    Queries Quality and Percentage

---
+++ 1211.sql
@@ -0,0 +1,32 @@
+select query_name, round(avg(rating/position), 2) as quality,
+round(
+    sum(case when rating < 3 then 1 else 0 end)/count(*) * 100,
+    2
+) as poor_query_percentage
+from queries
+where query_name is not null
+group by query_name
+
+def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
+    queries["quality"] = queries.apply(
+        lambda row: row["rating"]/row["position"] + 1e-10,
+        axis=1,
+    )
+    quality = queries.groupby(["query_name"])["quality"].mean().round(2).rename("quality").reset_index()
+    size = queries.groupby(["query_name"]).size().rename("size").reset_index()
+    poor = queries.query("rating < 3").groupby(["query_name"]).size().rename("poor").reset_index()
+    res = size.merge(
+        poor,
+        how="left",
+        on=["query_name"],
+    )
+    res["poor_query_percentage"] = res.apply(
+        lambda row: round(row["poor"]/row["size"]*100, 2),
+        axis=1,
+    )
+    res = res.merge(
+        quality,
+        how="outer",
+        on=["query_name"],
+    ).fillna(0)
+    return res[["query_name", "quality", "poor_query_percentage"]]
