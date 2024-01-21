bc839e2c3c95ad046964f732ed31690be4df20c3
ian0521 <iansu.twn@gmail.com>
2024-01-21 20:50:17 +0800

    Percentage of Users Attended a Contest

---
+++ 1633.sql
@@ -0,0 +1,24 @@
+select
+    contest_id,
+    round(count(r.user_id)/(select count(distinct user_id) from users) * 100, 2) as percentage
+from users u
+left join register r
+on r.user_id = u.user_id
+where contest_id is not null
+group by contest_id
+order by percentage desc, contest_id asc
+
+def users_percentage(
+    users: pd.DataFrame,
+    register: pd.DataFrame
+) -> pd.DataFrame:
+    int_all_students = len(users["user_id"].unique())
+    res = register.groupby(["contest_id"]).size().rename("count").reset_index()
+    res["percentage"] = res.apply(
+        lambda row: ((row["count"]/int_all_students)*100).round(2),
+        axis=1
+    )
+    return res[["contest_id", "percentage"]].sort_values(
+        ["percentage", "contest_id"],
+        ascending=[False, True]
+    )
