76bc6882ce058c87f0e5401dbf03562ddf50f1e1
ian0521 <iansu.twn@gmail.com>
2024-01-22 22:23:17 +0800

    add multiple python solutions

--- 1174.sql
+++ 1174.sql
@@ -12,10 +12,16 @@ from data
 where rn = 1
 
 def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    -- method 1: less efficient when data is large
     res = delivery.sort_values(by=["customer_id", "order_date"], ascending=[True, True])
     res = res.groupby(["customer_id"]).agg({
         "order_date": "first"
     }).reset_index()
+    -- method 2: more efficient
+    res = delivery.groupby(["customer_id"]).agg({
+        "order_date": "min"
+    }).reset_index()
+    
     res = res.merge(
         delivery,
         how="left",
@@ -25,3 +31,9 @@ def immediate_food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
         immediate = lambda row: (row.order_date == row.customer_pref_delivery_date).astype(int)
     )
     return pd.DataFrame(data={(res["immediate"].mean()*100).round(2)}, columns=["immediate_percentage"])
+
+    -- method 3
+    df_temp = df.sort_values(["customer_id", "order_date"], ascending=[True, True])
+    df_group = df_temp.groupby(["customer_id"]).head(1)
+    immediate_percentage = len(df_group[df_group["order_date"] == df_group["customer_pref_delivery_date"]])/len(df_group)
+    return pd.DataFrame({"immediate_percentage":[round(immediate_percentage * 100,2)]})
