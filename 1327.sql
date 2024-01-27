6c3fa1209a236fbb43ef40bdc3039d191cef1c85
ian0521 <iansu.twn@gmail.com>
2024-01-27 22:53:58 +0800

    List the Products Ordered in a Period

---
+++ 1327.sql
@@ -0,0 +1,27 @@
+with data as (
+    select
+        p.product_name, date_format(o.order_date, "%Y-%m") month, sum(unit) unit
+    from products p
+    join orders o on p.product_id = o.product_id
+    group by p.product_name, date_format(o.order_date, "%Y-%m")
+)
+select d.product_name, d.unit
+from data d
+where month = '2020-02'
+and unit >= 100
+
+def list_products(
+    products: pd.DataFrame,
+    orders: pd.DataFrame,
+) -> pd.DataFrame:
+    res = products.merge(
+        orders,
+        how="inner",
+        on=["product_id"],
+    )
+    res["order_date"] = res["order_date"].dt.strftime("%Y-%m")
+    res = res.groupby(["product_name", "order_date"]).agg({
+        "unit": "sum"
+    }).reset_index()
+    res = res.query("unit >= 100 and order_date == '2020-02'")
+    return res[["product_name", "unit"]]
