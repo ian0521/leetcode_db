b6dfc9e269ef43e93215a2d529fb6a8774af3e65
ian0521 <iansu.twn@gmail.com>
2024-01-27 22:10:03 +0800

    add another pandas solution

--- 1517.sql
+++ 1517.sql
@@ -2,8 +2,17 @@ select * from users
 where mail regexp '^[A-Za-z][A-Za-z0-9._-]*@leetcode[.]com'
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    -- method 1
     res = users.loc[
         lambda row: row.mail.str.contains(
+            r"^[A-Za-z][A-Za-z0-9._-]*@leetcode[.]com$"
+        )
+    ]
+    return res
+
+    -- method 2
+    res = users[
+        users["mail"].str.match(
             r"^[A-Za-z][A-Za-z0-9._-]*@leetcode[.]com$"
         )
     ]
