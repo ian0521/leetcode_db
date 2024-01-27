2661cabfef6ef7bee187362e8ff21b142e00a97d
ian0521 <iansu.twn@gmail.com>
2024-01-27 22:06:42 +0800

    Find Users With Valid E-Mails

---
+++ 1517.sql
@@ -0,0 +1,10 @@
+select * from users
+where mail regexp '^[A-Za-z][A-Za-z0-9._-]*@leetcode[.]com'
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    res = users.loc[
+        lambda row: row.mail.str.contains(
+            r"^[A-Za-z][A-Za-z0-9._-]*@leetcode[.]com$"
+        )
+    ]
+    return res
