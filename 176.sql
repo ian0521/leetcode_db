80e399ce4b2270bc5a119783d1ee2690a2fb610e
ian0521 <iansu.twn@gmail.com>
2024-01-28 14:57:32 +0800

    add another python solution

--- 176.sql
+++ 176.sql
@@ -9,6 +9,7 @@ where salary < (
 def second_highest_salary(
     employee: pd.DataFrame
 ) -> pd.DataFrame:
+    -- method 1
     max_salary = employee["salary"].max()
     employee = employee.query(
         f"""salary < {max_salary}"""
@@ -16,4 +17,16 @@ def second_highest_salary(
     salary = employee["salary"].max()
     return pd.DataFrame(data={
         "SecondHighestSalary": [salary],
+    })
+
+    -- method 2
+    unique_salary = employee["salary"].sort_values(
+        ascending=False
+    ).drop_duplicates()
+    if len(unique_salary) < 2:
+        return pd.DataFrame(data={
+            "SecondHighestSalary": [None],
+        })
+    return pd.DataFrame(data={
+        "SecondHighestSalary": [unique_salary.iloc[1]]
     })
